b3fb239ed3e9ea5a52eb3e68c705348df53c0575
Bogumil <najmobogi@gmail.com>
2024-11-30 18:00:28 +0100

    test

---
+++ app/src/main/java/com/example/moviebooktracker/Media.kt
@@ -0,0 +1,8 @@
+package com.example.moviebooktracker
+
+data class Media(
+    val title: String, // Tytuł filmu/książki
+    val creator: String, // Autor książki lub Reżyser filmu
+    val year: Int, // Rok wydania
+    val rating: Float // Ocena (np. 0.0 - 5.0)
+)
